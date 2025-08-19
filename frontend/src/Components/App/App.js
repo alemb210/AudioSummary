a3b213d6834b697c2f9f6a6f47ca50cf6a03260f
alem <alembicic210@gmail.com>
2025-08-18 22:01:16 -0400

    testing CI/CD

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Upload an audio file to be summarized.
+          Upload an audio file to be summarized!
         </p>
         <Upload />
       </header>
